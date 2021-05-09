7a7f9843f7893ccbf74b6bbfc462058a2e85d2f6
Piotr Jakubowski <piotrjakubowski@MacBook-Pro-Piotr.local>
2021-05-09 18:45:18 +0200

    Create test class

---
+++ TestClass.java
@@ -0,0 +1,13 @@
+public class TestClass {
+    int change;
+    int secondChange;
+
+    public TestClass() {
+        change = 1;
+        secondChange = 10
+    }
+
+    public newFunction(Boolean change) {
+        this.change = change;
+    }
+}
